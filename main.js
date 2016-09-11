227d065f3a5c4e5a1f1547532c57742d38d48046
ConnorTheFox <cameronclapp11@gmail.com>
2016-09-11 01:14:27 -0600

    Changed picture generation & inline airport status

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const airports = require('./src/airport.js')
 const returns = require('./src/returns.js')
 const log = require('./src/logController.js')
 
-const start = 'Use /countdown to start the MFF countdown!\nBot made by @ConnorTheFox.\nAll photos are taken by [AoLun](https://www.flickr.com/photos/aolun/)\nUse /help for command info.'
+const start = 'Use /countdown to start the MFF countdown!\nUse /airport and a airport code to get notifications about airport conditions before MFF\nBot made by @ConnorTheFox.\n\nUse /help for command info.'
 const help = 'Normal Commands:\n/countdown: Subscribes you do a daily reminder of how many days until MFF.\n/stopcountdown: Unsubscribes you from the daily countdown.\n/airport (code): Put in airport code and a couple days before the bot will monitor it for delays.\n/stopairport (code): Use this command to delete the monitor\n/info: Gives information about the techinial side of the bot.\n/daysleft: Tells you how many days are left.\n/howfar: Send the bot a location and it will map it and tell you how long it will take to get there by driving\n\nInline Queries: COMMING SOON'
 const info = 'Information about this bot:\nIt is coded in node.js\nIf you want to view the code goto https://github.com/ConnorTheFox/FurFestBot\nI made this bot because im hyped about MFF'
 let timeDrift = true
@@ -34,12 +34,14 @@ bot.on('new_chat_participant', function(msg) {
             bot.sendMessage(msg.chat.id, start, {parse_mode: 'markdown'})
         }
 })
-bot.on('migrate_to_chat_id', function(msg){
-      db.lookup('chatId', msg.migrate_from_chat_id).then(function(found) {
-          if (found[0]) {
-              db.update(found[0], 'chatId', msg.migrate_to_chat_id, 'finding', 'tag', 'name', found[0].name, 'group', 'true').then(console.log('Supergroup' + msg.chat.title + 'Updated'))
-          }
-      })
+bot.on('msg', function(msg){
+  if (msg.migrate_from_chat_id) {
+    db.lookup('chatId', msg.migrate_from_chat_id).then(function(found) {
+        if (found[0]) {
+            db.update(found[0], 'chatId', msg.migrate_to_chat_id, 'finding', 'tag', 'name', found[0].name, 'group', 'true').then(console.log('Supergroup' + msg.chat.title + 'Updated'))
+        }
+    })
+  }
 })
 
 
@@ -197,8 +199,24 @@ bot.onText(/\/help/, function(msg, match) {
     bot.sendMessage(msg.chat.id, help)
 })
 
-//bot.on('inline_query', function(msg) {
-//})
+bot.on('inline_query', function(msg) {
+  let testForCode = /^.../
+  if (testForCode.test(msg.query) === true) {
+    request('https://services.faa.gov/airport/status/' + msg.query + '?format=json', {method: 'HEAD'},  function(err, res, good2){
+      if (res.statusCode != 404) {
+        request('https://services.faa.gov/airport/status/' + msg.query + '?format=json', function(err, res, good){
+          let shouldCheck = JSON.parse(good)
+          if (shouldCheck.delay === 'false') {
+            bot.answerInlineQuery(msg.id, [{type: 'article', id: Math.random().toString().slice(0, 35), title: shouldCheck.IATA+': No Delay', input_message_content: {message_text:'No Delay'}}])
+          }
+          else {
+            bot.answerInlineQuery(msg.id, [{type: 'article', id: Math.random().toString().slice(0, 35), title: shouldCheck.IATA+': Delay! Click here for more info', input_message_content: {message_text:returns.delayCalc(shouldCheck)}}])
+          }
+        })
+      }
+    })
+  }
+})
 
 bot.onText(/\/airport (.+)/, function(msg, match) {
   if (msg.chat.title) {
@@ -293,7 +311,7 @@ function sendDaily() {
                             if (users[n].group === false) {
                                 log.daily('Message Sent To: ' + info.chat.first_name)
                             } else {
-                                log.daily('Message Sent To: ' + info.chat.title)
+                                log.daily('Message Sent To: ' + info.chat.title + info.chat.username)
                             }
                         }).catch(function(err) {
                             if (err) {
@@ -314,4 +332,3 @@ function checkDelays() {
     }
   })
 }
-
--- src/pic.js
+++ src/pic.js
@@ -3,67 +3,41 @@
 const path = require('path')
 const gm = require('gm')
 const fs = require('fs')
-const num = require('./urls.js')
 const RandomOrg = require('random-org')
 const days = require('./days.js')
-const random = new RandomOrg({
-    apiKey: fs.readFileSync(path.join(__dirname, "../private/randomkey.txt"), 'utf8').toString()
-})
 const log = require('./logController.js')
+const Trianglify = require('trianglify');
+const random = require('randomcolor')
 
 exports.pickImage = function() {
     return new Promise(function(res, rej) {
-        let randNum
-        random.generateIntegers({
-                min: 1,
-                max: 1181,
-                n: 1
-            })
-            .then(function(result) {
-                randNum = result.random.data[0]
-                findSize()
-            });
-
-        function findSize() {
-            gm(num.pics[randNum]).size(function(err, num) {
-                if (!err) {
-                    writeImage(num.width, num.height)
-                } else {
-                    console.log(err)
+      days.untilMff().then(day => {
+        var types = ['bright', 'dark']
+        var pattern = Trianglify({width: 1024, height: 1024, variance: "1", cell_size: 150, x_colors: random({count: 5, luminosity: Math.floor(Math.random()*3), hue: 'random'})}).png()
+                let dayStr
+                if (day === 1) {
+                  dayStr = '(Tomorrow Is\nMFF!)'
                 }
-            })
-        }
-
-        function writeImage(width, height) {
-            days.untilMff().then(function(day) {
-              let dayStr
-              if (day === 1) {
-                dayStr = 'Tomorrow Is\nMFF!'
-              }
-              else {
-                dayStr = day + ' days\nuntil MFF!'
-              }
-                let random1 = Math.floor(Math.random() * 255)
-                let random2 = Math.floor(Math.random() * 255)
-                let random3 = Math.floor(Math.random() * 255)
-                gm(num.pics[randNum])
-                    .fill('rgb(' + random1.toString() + ', ' + random2.toString() + ', ' + random3.toString() + ')')
-                    .drawText(width / 6, height / 3, dayStr)
-                    .font(path.join(__dirname, "../fonts/swag.ttf"))
-                    .fontSize(width / 5)
-                    .write(path.join(__dirname, "../Countdown/" + day.toString() + '.jpg'), function(err) {
-                        if (!err) {
-                            log.picture('Day ' + day + ' Generated')
-                            let file = fs.readFile(path.join(__dirname, "../Countdown/" + day + '.jpg'), function(err, data) {
-                                if (!err) {
-                                    res(data)
-                                } else {
-                                    rej(err)
-                                }
-                            })
-                        }
-                    })
-            })
-        }
+                else {
+                  dayStr = '{}' + day + ' days{}\n^until MFF!^'
+                }
+                  gm(new Buffer(pattern.replace(/^data:image\/png;base64,/, ''), 'base64'))
+                      .fill('rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')')
+                      .drawText('50', '412', dayStr)
+                      .font(path.join(__dirname, "../fonts/font.ttf"))
+                      .fontSize('150')
+                      .write(path.join(__dirname, "../Countdown/" + day.toString() + '.png'), function(err) {
+                          if (!err) {
+                              log.picture('Day ' + day + ' Generated')
+                              let file = fs.readFile(path.join(__dirname, "../Countdown/" + day + '.png'), function(err, data) {
+                                  if (!err) {
+                                      res(data)
+                                  } else {
+                                      rej(err)
+                                  }
+                              })
+                          }
+                      })
+      })
     })
 }
