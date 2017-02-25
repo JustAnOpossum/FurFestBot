5d02d23851da459eab410fd990a69cfc03e8a7af
Connor <cameronclapp11@gmail.com>
2017-02-25 16:09:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let bot
-const process = require('process') //wow
+const process = require('process')
 const child = require('child_process')
 const path = require('path')
 const emoji = require('node-emoji')
