eec9fc13858a742886d8bc6519ea96df9c98939b
Connor <fox@nerdfox.me>
2017-12-13 17:16:23 -0700

    Changed Messages

--- src/message.js
+++ src/message.js
@@ -6,11 +6,12 @@ module.exports = {
       Info: 'info',
       'Days Left': 'daysleft'
    },
-   info: 'This bot is made by @ConnorTheFox, This bot is running for the second year in the row. It ran for MFF 2016 and is running for MFF 2017. If you want to look at the source code go here: https://github.com/ConnorTheFox/FurFestBot',
-   start: 'This bot counts down the days until MFF! \nUse the buttons below to start the countdown. This bot will send a daily picture with how many days are left until MFF 2017!',
-   startGroup: 'This bot counts down the days until MFF! \nuse /countdown to subscribe. This bot will send a daily picture with how many days are left until MFF 2017!',
+   info: 'This bot is made by @ConnorTheFox, This bot will run for MFF 2018. It ran for MFF 2016 and MFF 2017. If you want to look at the source code go here: https://github.com/ConnorTheFox/FurFestBot',
+   start: 'Thank you all for using the bor form MFF 2017. It will start again sometime in 2018.',
+   startGroup: 'Thank you all for using the bor form MFF 2017. It will start again sometime in 2018.',
    countdown: {
-      added: 'Added to daily countdown. The picture is sent every day at:\n9AM PST\n10AM MST\n11AM CST\n12PM EST',
+      added: 'Subscribed, thank you!',
       removed: 'Removed from daily countdown.'
    }
 }
+//The picture is sent every day at:\n9AM PT\n10AM MT\n11AM CT\n12PM ET
